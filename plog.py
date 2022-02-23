008437b8a9057060c26344d4a1cd6036b85f97e4
Ryan Mcmanus <35876565+r-j-mcmanus@users.noreply.github.com>
2022-02-23 10:01:08 +0000

    initial commit
    nothing much

---
+++ plog.py
@@ -0,0 +1,13 @@
+import state
+
+class pLog():
+	"""
+	
+	"""
+
+	event_number = 0
+	event_times = []
+
+	__init__(self,  data):
+		event_number = eventNumber
+		event_times = table()
---
+++ state.py
@@ -0,0 +1,16 @@
+class state():
+	"""Contains the components of the data"""
+	lambda_1 = 0
+	lambda_2 = 0
+	t_change = 0
+	step_1 = 0
+	step_2 = 0
+
+	plog = 0
+
+	def __init__(self, l1=0, l2=0, tc=0, k1=0, k2=0):
+		lambda_1 = l1 
+		lambda_2 = l2
+		t_change = tc 
+		step_1 = k1
+		step_2 = k2 
